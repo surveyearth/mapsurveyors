dc9eeb33c8795c0d8f15e56f857276b8b9c06704
Survenator <landsurveyingjobber@gmail.com>
2017-08-14 19:40:19 -0400

    Update fusiontable_settings.js

--- fusiontable_settings.js
+++ fusiontable_settings.js
@@ -34,7 +34,7 @@ var MapsLib = MapsLib || {}; MapsLib.schemaVersion = 2;
 
     // *New Fusion Tables Requirement* API key. found at https://code.google.com/apis/console/
     // *Important* this key is for demonstration purposes. please register your own.
-    MapsLib.googleApiKey ="AIzaSyDQ2YbYTGXjvfeJEIM15XjkB3T8CoTbRSU";
+    MapsLib.googleApiKey ="AIzaSyAR5eeFETe-sMigouZtPABzHpyA02MGyjQ";
     
 
     // DONE!  YOU COULD DELETE EVERYTHING AFTER THIS POINT AND STILL HAVE A WORKING APP.
